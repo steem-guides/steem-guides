ca52f1cd56b5d495037494fd11634c6955d31773
Thinker <mstr.raphael@gmail.com>
2019-04-01 18:53:49 +0800

    add the links to pdf and epub download files

--- scripts/_build.sh
+++ scripts/_build.sh
@@ -2,8 +2,9 @@
 
 set -ev
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 # add the update to fix ctexbook too old issue
 tlmgr update --self --all --reinstall-forcibly-removed
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+# make gitbook the last target to add the links to pdf and epub
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
