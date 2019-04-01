6f61a27d633f5e390d382f6809ea439228343d49
Thinker <mstr.raphael@gmail.com>
2019-04-01 16:58:06 +0800

    update the build script with comment

--- _build.sh
+++ _build.sh
@@ -3,6 +3,7 @@
 set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+# add the update to fix ctexbook too old issue
 tlmgr update --self --all --reinstall-forcibly-removed
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
